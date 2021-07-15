8550f6bbe5cd4824e652b4c1dac85cb3b0aafc2b
acatzk <joshuaimalay@gmail.com>
2021-07-15 16:48:44 +0800

    📱 RES: Mobile responsive design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,10 +9,10 @@ const Index: React.FC<{}> = () => {
       <Head>
         <title>Stats Preview Card Component</title>
       </Head>
-      <div className="antialiased min-h-screen w-full bg-[#0A0C1B]">
-        <div className="flex items-center justify-center h-screen">
-          <div className="flex flex-row-reverse rounded-md overflow-hidden bg-[#1C1938] max-w-4xl h-80">
-            <div className="relative w-1/2 h-auto">
+      <div className="antialiased bg-[#0A0C1B]">
+        <div className="flex items-center justify-center h-full min-h-screen w-full">
+          <div className="flex flex-col md:flex-row-reverse rounded-md overflow-hidden mx-6 xs:mx-8 bg-[#1C1938] max-w-4xl h-auto md:h-80">
+            <div className="relative w-auto md:w-1/2 h-80 md:h-auto">
               <Image
                 src="/images/image-header-desktop.jpg"
                 layout="fill"
@@ -20,19 +20,19 @@ const Index: React.FC<{}> = () => {
               />
               <span className="absolute bg-[#A952E2] inset-0 bg-opacity-60 contrast-125 brightness-50 saturate-75"></span>
             </div>
-            <div className="w-1/2 flex flex-col my-5 mx-6 space-y-6">
+            <div className="w-auto md:w-1/2 flex flex-col my-5 mx-0 md:mx-6 space-y-0 md:space-y-6 max-w-md md:max-w-none">
               <div className="space-y-4 p-6 max-w-3xl">
-                <h1 className="text-white font-semibold text-3xl leading-tight">
+                <h1 className="text-white font-semibold text-center md:text-left text-2xl md:text-3xl leading-tight">
                   Get <span className="text-[#A952E2]">insight</span> that help
                   your business grow.
                 </h1>
-                <p className="text-xs text-[#9391AB] leading-normal pr-6">
+                <p className="text-xs text-center md:text-left text-[#9391AB] leading-relaxed px-8 md:px-0 pr-0 md:pr-6">
                   Discover the benefits of data analytics and make better
                   decisions regarding revenue, customer experience, and overall
                   efficiency.
                 </p>
               </div>
-              <div className="flex items-center my-5 mx-6 space-x-10 text-white">
+              <div className="flex flex-col md:flex-row items-center my-0 md:my-5 mx-0 md:mx-6 space-x-0 md:space-x-10 space-y-4 md:space-y-0 text-white">
                 {stats.map(({ count, label }, i) => (
                   <Stats key={i} count={count} label={label} />
                 ))}
@@ -47,7 +47,7 @@ const Index: React.FC<{}> = () => {
 
 const Stats: React.FC<IStat> = ({ count, label }) => {
   return (
-    <div className="flex flex-col space-y-1">
+    <div className="flex flex-col items-center space-y-0 md:space-y-1 w-full">
       <h2 className="font-bold">{count}</h2>
       <p className="text-xs font-medium text-[#7A788E] uppercase">{label}</p>
     </div>
