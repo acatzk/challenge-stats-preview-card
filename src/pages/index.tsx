89da0193466aa8f989bd234f9919260065341377
acatzk <joshuaimalay@gmail.com>
2021-07-15 14:51:41 +0800

    ✨ FEATURE: Tailwindcss setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Index: React.FC<{}> = () => {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Stats Preview Card Comopnent</title>
+      </Head>
+      <div className="text-2xl font-bold text-indigo-600">Hi Gayle</div>
+    </React.Fragment>
+  )
+}
+
+export default Index
